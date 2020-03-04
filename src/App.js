ed15d3be4514d2a16a4fc0de8d36a06e77fb556b
ericdiedrich <ericdiedrich@users.noreply.github.com>
2020-03-04 14:50:03 +0000

    testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Search from './components/search'
+import Nav from './components/navbar'
+import Main from './components/main'
 
 function App() {
   return (
     <div>
+      <Nav />
+      <Main />
       <Search />
     </div>
   );
