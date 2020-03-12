f3b979393e2f3585af265a7880a8291c20dd5a1b
MatthewGuyRobin <matthewguyrobinson@gmail.com>
2020-03-12 14:00:02 +0000

    states added who to follow

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -11,7 +11,7 @@ import SubmitTxtBox from "./SubmitTxtBox"
 class Textinput extends Component {
     state = {
         posts: [
-            {profilepic: profilepic, user: "adam22", content: "What is with this coronavirus ?" },
+            { profilepic: profilepic, user: "adam22", content: "What is with this coronavirus ?" },
         ],
         currentPost: { profilepic: profilepic, user: "adam22", content: "" }
     }
@@ -22,52 +22,53 @@ class Textinput extends Component {
         })
     }
 
-    addPostHandler = (e) => { 
-      if (e.key === "Enter"){
-        this.setState({
-            posts: [...this.state.posts, this.state.currentPost],
-            currentPost: { profilepic: profilepic, user: "", content: e.target.value= "" },
-        })}
+    addPostHandler = (e) => {
+        if (e.key === "Enter") {
+            this.setState({
+                posts: [...this.state.posts, this.state.currentPost],
+                currentPost: { profilepic: profilepic, user: "", content: e.target.value = "" },
+            })
+        }
     }
 
     render() {
         const allPosts = this.state.posts.map((posts, index) => {
             return <div key={index} className="currentpostContainer">
-              <div className="textpostContainer">
-                <div className="likeContainer">
-                    <img className="likeImg" src={Heart} alt="heart" ></img>
-                    <h4 className="likeUser">adam22 liked</h4>
-                </div>
-                <div className="textpostFlexWrapper">
-                    <div>
-                        <img className="profileImg" src={posts.profilepic} alt="pic"></img>
+                <div className="textpostContainer">
+                    <div className="likeContainer">
+                        <img className="likeImg" src={Heart} alt="heart" ></img>
+                        <h4 className="likeUser">adam22 liked</h4>
                     </div>
-                    <div className="max-widthContainter">
-                        <div className="flexUser">
-                            <h2 className="htagMargin" >adam22</h2>
-                            <h2 className="htagMargin" >@adam22</h2>
-                            <h4 className="htagMargin">.</h4>
-                            <h2 className="htagMargin">8h</h2>
-                        </div>
+                    <div className="textpostFlexWrapper">
                         <div>
-                            <p className="postContent">{posts.content}</p>
+                            <img className="profileImg" src={posts.profilepic} alt="pic"></img>
                         </div>
-                        <div className="TweetImageFlexWrapper">
-                            <img className="tweetlogo" alt="logo" src={commentlogo} ></img>
-                            <img className="tweetlogo" alt="logo" src={retweetlogo} ></img>
-                            <img className="tweetlogo" alt="logo" src={heartlogo} ></img>
-                            <img className="tweetlogo" alt="logo" src={sharelogo} ></img>
+                        <div className="max-widthContainter">
+                            <div className="flexUser">
+                                <h2 className="htagMargin" >adam22</h2>
+                                <h2 className="htagMargin" >@adam22</h2>
+                                <h4 className="htagMargin">.</h4>
+                                <h2 className="htagMargin">8h</h2>
+                            </div>
+                            <div>
+                                <p className="postContent">{posts.content}</p>
+                            </div>
+                            <div className="TweetImageFlexWrapper">
+                                <img className="tweetlogo" alt="logo" src={commentlogo} ></img>
+                                <img className="tweetlogo" alt="logo" src={retweetlogo} ></img>
+                                <img className="tweetlogo" alt="logo" src={heartlogo} ></img>
+                                <img className="tweetlogo" alt="logo" src={sharelogo} ></img>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
         }
         )
 
         return (<div>
-            <SubmitTxtBox recordtweet={this.recordPostHandler} tweet={this.addPostHandler}/>
+            <SubmitTxtBox recordtweet={this.recordPostHandler} tweet={this.addPostHandler} />
             {/* <input onChange={this.recordPostHandler} value={this.state.currentPost.content}></input> */}
             {/* <button onClick={this.addPostHandler}>Submit</button> */}
             <div className="textpostContainer">
